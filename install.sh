d79d2805c5446d6ced275e7ccd0fb5d51b55406b
masted <masted311@gmail.com>
2013-06-23 21:32:47 +0400

    ...

--- install.sh
+++ install.sh
@@ -18,6 +18,6 @@ ssh ${slave} << EOF
   apt-get -y install bind9 php5-cli
   cd /etc/bind
   conf="\tallow-transfer { 127.0.0.1; };\n\tallow-recursion { 127.0.0.1; };"
-  php -r "file_put_contents(preg_replace('/^};\n/m', \"${conf}\n};\", file_get_contents('named.conf.options'));"
+  php -r "file_put_contents(preg_replace('/^};\n/m', \"${conf}\n};\", file_get_contents('named.conf.options')));"
   rndc reload
 EOF
