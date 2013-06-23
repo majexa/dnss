18663ee10bd9fb992306da345cd66c386dc312d2
masted <masted311@gmail.com>
2013-06-23 19:12:12 +0400

    ...

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 apt-get -y install python-software-properties
 apt-get update
 add-apt-repository --yes ppa:ondrej/php5
-apt-get install php5-cli
+apt-get -y install php5-cli
 
 echo "Input slave server IP"
 read slaveIp
@@ -12,11 +12,10 @@ php -r "print preg_replace('/^};\n/m', \"${conf}\n};\", file_get_contents('named
 rndc reload
 
 ssh-keygen
-slaveIp=37.139.3.229
 scp /root/.ssh/id_rsa.pub ${slaveIp}:/root/.ssh/authorized_keys
 
 ssh ${slaveIp}
-apt-get install bind9 php5-cli
+apt-get -y install bind9 php5-cli
 cd /etc/bind
 conf="\tallow-transfer { 127.0.0.1; };\n\tallow-recursion { 127.0.0.1; };"
 php -r "print preg_replace('/^};\n/m', \"${conf}\n};\", file_get_contents('named.conf.options'));"
